faebe7e35c57187676e54396fd70adf4a6fa36d2
Andreas Zhang <denglitsch@gmail.com>
2023-05-24 11:00:58 +0800

    feat: port to 2cg

--- vivado/bd/system.tcl
+++ vivado/bd/system.tcl
@@ -43,7 +43,7 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 
 set list_projs [get_projects -quiet]
 if { $list_projs eq "" } {
-   create_project project_1 myproj -part xczu3eg-sfvc784-1-i
+   create_project project_1 myproj -part xczu2cg-sfvc784-1-e
 }
 
 
